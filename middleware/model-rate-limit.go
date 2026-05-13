bcfc46aaad0efa0b1e66fd15f44d7e07de75564d
Mino-dev-ceo <Mino-dev-ceo@users.noreply.github.com>
2026-05-13 21:49:57 +0800

    fix: skip rate limit for async image tasks

--- middleware/model-rate-limit.go
+++ middleware/model-rate-limit.go
@@ -166,7 +166,7 @@ func memoryRateLimitHandler(duration int64, totalMaxCount, successMaxCount int) 
 // ModelRequestRateLimit 模型请求限流中间件
 func ModelRequestRateLimit() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		if c.GetHeader("X-Mino-Async-Image-Task") != "" {
+		if shouldSkipModelRequestRateLimit(c) {
 			c.Next()
 			return
 		}
@@ -200,5 +200,17 @@ func ModelRequestRateLimit() func(c *gin.Context) {
 		} else {
 			memoryRateLimitHandler(duration, totalMaxCount, successMaxCount)(c)
 		}
+	}
+}
+
+func shouldSkipModelRequestRateLimit(c *gin.Context) bool {
+	if c.GetHeader("X-Mino-Async-Image-Task") != "" {
+		return true
+	}
+	switch c.Request.URL.Path {
+	case "/v1/images/generations/async", "/v1/images/edits/async":
+		return true
+	default:
+		return false
 	}
 }
